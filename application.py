369df3d28b9ea94c9fde5f76d0fbe3d1a94dee61
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2023-03-24 22:17:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -27,11 +27,17 @@ def result():
             options.add_argument("--headless")
 #             chrome_driver_path = os.environ.get('CHROME_DRIVER_PATH')
 #             print(chrome_driver_path)
-
+                
 #             driver = webdriver.Chrome(executable_path='/usr/var/app/current/chromedriver.exe',options=options)
 #             driver = webdriver.Chrome(executable_path=chrome_driver_path,options=options)
 #             chrome_driver_path = os.environ.get('CHROME_DRIVER_PATH')
-            driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.binary_location = '/usr/bin/google-chrome'
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+
+
+            driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=chrome_options)
             
             yt = searchString
             driver.get(yt)
