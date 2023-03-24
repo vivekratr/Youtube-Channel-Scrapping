f139d7a762cfe19e0072454fd7f81cca6134c638
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2023-03-24 23:06:53 +0530

    Update application.py

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ def result():
             chrome_options.binary_location = '/usr/bin/google-chrome'
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
-
+            chrome_options.add_argument('--disable-dev-shm-usage')
 
             driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=chrome_options)
             
