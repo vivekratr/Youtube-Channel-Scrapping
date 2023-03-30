b930f0ff0cbaa25fff8b8a7593b73e9a7069cf3f
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2023-03-30 15:18:38 +0530

    Update application.py

--- application.py
+++ application.py
@@ -36,6 +36,7 @@ def result():
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument("--disable-extensions")
 
             driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=chrome_options)
             
