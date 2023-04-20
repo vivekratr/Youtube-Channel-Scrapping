5ca6a2651ea8c96d91c3f3a5a5960983be786571
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2023-04-20 17:44:58 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-# from flask import Flask  
+# from flask import Flask    
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
